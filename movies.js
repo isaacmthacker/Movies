dfbad7fe5599baac38190d306dc4792b3919e423
Isaac <ithacker@trinity.edu>
2016-05-24 14:25:41 -0500

    Added years

--- movies.js
+++ movies.js
@@ -2,6 +2,7 @@
 var numMovies = -1;
 var actor = "";
 var sorted = false;
+
 function searchActor() {
   sorted = false;
   $("#results").remove();
@@ -11,6 +12,7 @@ function searchActor() {
 
   $.get("retrieveMovies.php?actor=" + actor, function(data) {
     var movies = {};
+    var years = [];
     if(data == "ERROR") console.log("ERROR");
     var json = JSON.parse(data);
     var pageId = 0;
@@ -21,21 +23,25 @@ function searchActor() {
     var splitText = text.split("\n");
     var films = grabFilms(splitText);
     //For debugging
-    /*
+   /* 
     for(f in films) {
       $("body").append("<p>" + films[f] + "</p>");
     }
     */
-    var parsedFilms = parseFilms(films, movies);
+    
+    var parsedFilms = parseFilms(films, movies, years);
 
-    var numMovies = 1;
     $("body").append("<table id='results'></table>");
     $("#results").append("<caption>" + actor + "</caption>");
-    $("#results").append("<thead><tr><td>#</td><td>Title</td><td>Box Office</td></tr></thead>");
-    for(film in parsedFilms) {
-      var title = parsedFilms[film];
-      $("#results").append("<tr id=" + mojoFriendlyString(title) + "><th>" + numMovies + "</th><th>" + title + "</th></tr>");
-      numMovies += 1;
+    $("#results").append("<thead><tr><td>Year</td><td>Title</td><td>Box Office</td></tr></thead>");
+    parsedFilmsArr = [];
+    for(p in parsedFilms) {
+      parsedFilmsArr.push(parsedFilms[p]);
+    }
+
+    for(var i = 0; i < parsedFilmsArr.length; ++i) {
+      var title = parsedFilmsArr[i];
+      $("#results").append("<tr id=" + mojoFriendlyString(title) + "><th>" + years[i] + "</th><th>" + title + "</th></tr>");
     }
 
     getBoxOfficeAmt(movies);
@@ -69,9 +75,12 @@ function grabFilms(splitText) {
 
 
 //Gets results from grabFilms and parses out the film title
-function parseFilms(splitText, movies) {
+function parseFilms(splitText, movies, years) {
   numMovies = 1;
   for(var i = 0; i < splitText.length; ++i) {
+    if(splitText[i].match(/\|\s*\d{4}/)) {
+      years.push( splitText[i].match(/\d{4}/)[0] );
+    }
     var matchWithSingleQuotes = splitText[i].match(/''\[\[.*[A-z].*\]\]''/);
     if(!matchWithSingleQuotes) { 
       matchWithSingleQuotes = splitText[i].match(/''\{\{.*[A-z].*\}\}''/);
@@ -79,50 +88,63 @@ function parseFilms(splitText, movies) {
     var matchWithDoubleQuotes = splitText[i].match(/"\[\[.*[A-z].*\]\]"/);
     if(!matchWithDoubleQuotes) { 
       matchWithDoubleQuotes = splitText[i].match(/"\{\{.*[A-z].*\}\}"/);
-    }
-    if(!(matchWithSingleQuotes || matchWithDoubleQuotes)) {
-      delete splitText[i];
+      }
+    var matchJustSingleQuotes = splitText[i].match(/!scope="row"\|\s*''[A-z]+.*''/);
+    var matchJustDoubleQuotes = splitText[i].match(/!scope="row"\|\s*"[A-z]+.*"/);
+
+    if(!(matchWithSingleQuotes || matchWithDoubleQuotes || matchJustSingleQuotes || matchJustDoubleQuotes)) {
+        console.log("Deleted : " + splitText[i]);
+        delete splitText[i];
     } else {
-      if(matchWithSingleQuotes) {
-        splitText[i] = matchWithSingleQuotes[0].slice(4, matchWithSingleQuotes.length-5);
-      } else {
-        splitText[i] = matchWithDoubleQuotes[0].slice(3, matchWithDoubleQuotes.length-4);
-      }
-      splitText[i] = splitText[i].split("|");
-      if(splitText[i].length == 1) {
-        splitText[i] = splitText[i][0];
-      } else {
-        if(splitText[i].includes("sortname")) {
-          var title = "";
-          for(var j = 0; j < splitText[i].length; ++j) {
-            title += splitText[i][j];
-            title += " ";
-          }
-          title = title.replace("sortname", "");
-          title = title.replace("nolink=1", "");
-          title = title.replace(/dab=.*film/, "");
-          splitText[i] = title.trim();
+      if(matchWithSingleQuotes || matchWithDoubleQuotes) {
+        splitText[i] = parseFilmDoubleOrSingleQuotes(splitText[i], matchWithSingleQuotes, matchWithDoubleQuotes);
+      } else if(matchJustSingleQuotes) {
+          splitText[i] = splitText[i].match(/''[A-z].*''/)[0].replace(/'/g, "");  
         } else {
-          splitText[i] = splitText[i][splitText[i].length-1];
-        }
+          //matchJustDoubleQuotes
+          splitText[i] = splitText[i].match(/"[A-z].*"/)[0].replace(/"/g, "");  
       }
-      splitText[i] = splitText[i].replace("sort=", "");
-      splitText[i] = splitText[i].replace("]]", "");
+      if(splitText[i] && splitText[i].match(/\(.*film\)/)) {
+        splitText[i] = splitText[i].split("(")[0].trim();
+      }
       movies[splitText[i]] = 0;
       ++numMovies;
     }
-    if(splitText[i] && splitText[i].match(/\(.*film\)/)) {
-      delete movies[splitText[i]];
-      splitText[i] = splitText[i].split("(")[0].trim();
-      movies[splitText[i]] = 0;
-    }
-    /*if(splitText[i] == "nolink=1") {
-      delete movies[splitText[i]];
-      delete splitText[i];
-    }*/
   }
   console.log(numMovies);
+  console.log(years.length);
   return splitText;
+}
+
+
+function parseFilmDoubleOrSingleQuotes(curFilm, matchWithSingleQuotes, matchWithDoubleQuotes) {
+  var returnString = "";
+  if(matchWithSingleQuotes) {
+    returnString = matchWithSingleQuotes[0].slice(4, matchWithSingleQuotes.length-5);
+  } else {
+    returnString = matchWithDoubleQuotes[0].slice(3, matchWithDoubleQuotes.length-4);
+  }
+  returnString = returnString.split("|");
+  if(returnString.length == 1) {
+    returnString = returnString[0];
+  } else {
+    if(returnString.includes("sortname")) {
+      var title = "";
+      for(var j = 0; j < returnString.length; ++j) {
+        title += returnString[j];
+        title += " ";
+      }
+      title = title.replace("sortname", "");
+      title = title.replace("nolink=1", "");
+      title = title.replace(/dab=.*film/, "");
+      returnString = title.trim();
+    } else {
+      returnString = returnString[returnString.length-1];
+    }
+  }
+  returnString = returnString.replace("sort=", "");
+  returnString = returnString.replace("]]", "");
+  return returnString;
 }
 
 //Converts movie title into a string to use with Box Office Mojo api
